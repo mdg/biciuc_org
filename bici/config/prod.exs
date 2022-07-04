f650f6d690893b26a5fe1f43c801b3b520d8bbb2
matthew graham <mdg149@gmail.com>
2022-07-04 02:05:13 -0400

    dial back the redirects on non-https
    it's causing redirects to nowhere in prod

--- bici/config/prod.exs
+++ bici/config/prod.exs
@@ -45,7 +45,7 @@ config :bici, BiciWeb.Endpoint,
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :bici, BiciWeb.Endpoint,
-   force_ssl: [hsts: true]
+# config :bici, BiciWeb.Endpoint,
+#    force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
